7bdce977dcab7f3e8baf27d3f451349614df0771
Nick Kenworthy <xndt98@live.com>
2021-01-08 15:02:26 -0800

    specified how to get org functionality with elfeed

--- config.el
+++ config.el
@@ -18,7 +18,6 @@
 ;;
 ;; They all accept either a font-spec, font string ("Input Mono-12"), or xlfd
 ;; font string. You generally only need these two:
-(setq doom-font (font-spec :family "xos4 Terminus" :size 18))
 (setq doom-font (font-spec :family "Terminus" :size 18))
 
 ;; There are two ways to load a theme. Both assume the theme is installed and
