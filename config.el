eebcccadf020d077f502e19516d0b51c0744e74a
Nick Kenworthy <xndt98@live.com>
2021-02-28 18:59:45 -0800

    add org-crypt section

--- config.el
+++ config.el
@@ -225,3 +225,21 @@
 
 (sp-local-pair 'org-mode "\\frac{" "}{}"
                :trigger "\\cf")
+
+(require 'org-crypt)
+(org-crypt-use-before-save-magic)
+(setq org-tags-exclude-from-inheritance '("crypt"))
+
+;; (setq org-crypt-key nil)
+(setq org-crypt-key "xndt98@live.com")
+;; GPG key to use for encryption
+;; Either the Key ID or set to nil to use symmetric encryption.
+
+(setq auto-save-default nil)
+;; Auto-saving does not cooperate with org-crypt.el: so you need to
+;; turn it off if you plan to use org-crypt.el quite often.  Otherwise,
+;; you'll get an (annoying) message each time you start Org.
+
+;; To turn it off only locally, you can insert this:
+;;
+;; # -*- buffer-auto-save-file-name: nil; -*-
